f49275446d86e73ac779e8f38b52a0a1e3f59916
jtaylor2037 <jtaylor2037@comcast.net>
2025-03-06 12:55:08 -0600

    Restored auto_trading.py script

--- auto_trading.py
+++ auto_trading.py
@@ -1,1 +1,9 @@
 ﻿import time
+
+def main():
+    print("✅ Trading bot is running...")
+    time.sleep(5)
+    print("✅ Trading bot has finished execution.")
+
+if __name__ == "__main__":
+    main()
